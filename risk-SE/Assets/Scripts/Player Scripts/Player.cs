c91feeb57fb0939e256af37dd461b3f2179256a8
zaphod06 <161735977+zaphod06@users.noreply.github.com>
2024-03-12 15:39:02 +0000

    Added scripts and object for game
    Added first draft scripts for player, Card, Mission card, troops, territories after class diagram was made

---
+++ risk-SE/Assets/Scripts/Player Scripts/Player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Territories[] territories;
+    public int noTroops;
+    public MissionCards mission;
+    public Cards[] cards;
+    public string colour;
+    public int number;
+    public int turn;
+    public Dice_d6_Plastic Dice;
+    public Deck deck;
+    public Troops[] troops;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
